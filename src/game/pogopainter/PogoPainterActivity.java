7a75c8b84f0467e4367268791da5cf5a6b706070
Asen Lekov <asenlekoff@gmail.com>
2012-02-15 18:38:25 +0200

    Initial commit
    Creating Android project (SDK 7, Android 2.1)

---
+++ src/game/pogopainter/PogoPainterActivity.java
@@ -0,0 +1,13 @@
+package game.pogopainter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PogoPainterActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
