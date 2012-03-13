6d72988d2f48cf8b7f1a5f2c567eb005edf81782
Asen Lekov <Asen Lekov@localhost>
2012-03-13 22:49:59 +0200

    0.3.5(20120313) + added: Lang options + added: Initial bulgarian translate - bug: need to restart app to change completely lang

--- src/game/pogopainter/PogoPainterActivity.java
+++ src/game/pogopainter/PogoPainterActivity.java
@@ -20,26 +20,27 @@ public class PogoPainterActivity extends Activity implements OnClickListener {
 
 	private int reques_code = 1;
 	private String tag = "Pogo";
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		View aboutButton = findViewById(R.id.about_button);
+		aboutButton.setOnClickListener(this);
+
+		View multiPlayerButton = findViewById(R.id.multiPlayer_button);
+		multiPlayerButton.setOnClickListener(this);
+
+		View singlePlayerButton = findViewById(R.id.singlePlayer_button);
+		singlePlayerButton.setOnClickListener(this);
+
+		View optionsButton = findViewById(R.id.options_button);
+		optionsButton.setOnClickListener(this);
+	}
 	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        View aboutButton = findViewById(R.id.about_button);
-        aboutButton.setOnClickListener(this);
-        
-        View multiPlayerButton = findViewById(R.id.multiPlayer_button);
-        multiPlayerButton.setOnClickListener(this);
-        
-        View singlePlayerButton = findViewById(R.id.singlePlayer_button);
-        singlePlayerButton.setOnClickListener(this);
-        
-        View optionsButton = findViewById(R.id.options_button);
-        optionsButton.setOnClickListener(this);
-    }
-    
-    @Override
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
 		MenuInflater inflater = getMenuInflater();
@@ -77,7 +78,7 @@ public class PogoPainterActivity extends Activity implements OnClickListener {
 		}
 		return false;
 	}
-    
+
 	@Override
 	public void onBackPressed() {
 		new AlertDialog.Builder( this )
@@ -93,26 +94,26 @@ public class PogoPainterActivity extends Activity implements OnClickListener {
 			public void onClick(DialogInterface dialog, int which) {
 				Log.d( tag, "Exit - Positive");
 				finish();
-				
+
 			}
 		})
 		.show();
 	}
-    public void onClick(View v) {
-    	
-        switch (v.getId()) {
-        case R.id.about_button:
-        	Log.d( tag, "About");
-        	Intent About = new Intent(this, AboutActivity.class);
-            startActivity(About);
-            break;
-        case R.id.singlePlayer_button:
-        	Log.d( tag, "Singleplayer");
-        	Intent Single = new Intent(this, MultiplayerActivity.class);
-            startActivity(Single);
-            break;
-        case R.id.multiPlayer_button:
-        	BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+	public void onClick(View v) {
+
+		switch (v.getId()) {
+		case R.id.about_button:
+			Log.d( tag, "About");
+			Intent About = new Intent(this, AboutActivity.class);
+			startActivity(About);
+			break;
+		case R.id.singlePlayer_button:
+			Log.d( tag, "Singleplayer");
+			Intent Single = new Intent(this, MultiplayerActivity.class);
+			startActivity(Single);
+			break;
+		case R.id.multiPlayer_button:
+			BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 			if (mBluetoothAdapter == null) {
 				Log.d(tag, "BluetoothAlert");
 				new AlertDialog.Builder( this )
@@ -128,27 +129,27 @@ public class PogoPainterActivity extends Activity implements OnClickListener {
 			} else {
 				Log.d(tag, "Multiplayer");
 				Intent Multiplayer = new Intent(this, MultiplayerActivity.class);
-	            startActivity(Multiplayer);
+				startActivity(Multiplayer);
 			}
-            break;
-        case R.id.options_button:
-        	Log.d(tag, "Preferences");
-        	Intent Options = new Intent(this, PreferencesActivity.class);
-            startActivityForResult(Options, reques_code);
-            break;
-        }
-    }
-    
-    @Override
-    protected void onDestroy() {
-    	Log.d(tag, "Exiting");
-    	this.finish();
-    	super.onDestroy();
-    }
-    
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    	if (requestCode == reques_code) {
+			break;
+		case R.id.options_button:
+			Log.d(tag, "Preferences");
+			Intent Options = new Intent(this, PreferencesActivity.class);
+			startActivityForResult(Options, reques_code);
+			break;
+		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		Log.d(tag, "Exiting");
+		this.finish();
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == reques_code) {
 			if (resultCode == RESULT_OK) {
 				Log.d(tag, "Preferences changed");
 				Toast.makeText(getBaseContext(), "Changes saved", Toast.LENGTH_SHORT).show();
@@ -157,9 +158,5 @@ public class PogoPainterActivity extends Activity implements OnClickListener {
 				Toast.makeText(getBaseContext(), "Preferences reset", Toast.LENGTH_SHORT).show();
 			}
 		}
-    }
-    
-    public void getApplicationVersion() {
-		
 	}
 }
--- src/game/pogopainter/PreferencesActivity.java
+++ src/game/pogopainter/PreferencesActivity.java
@@ -1,5 +1,6 @@
 package game.pogopainter;
 
+import java.util.Locale;
 import java.util.Map;
 
 import android.app.AlertDialog;
@@ -9,6 +10,7 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
@@ -35,76 +37,90 @@ public class PreferencesActivity extends PreferenceActivity implements OnSharedP
 	@Override
 	public void onBackPressed() {
 		Log.d(tag, "Close");
-		//Toast.makeText(getBaseContext(), "Changes saved", Toast.LENGTH_SHORT).show();
 		finish();
 	}
-	
-	 @Override
-		public boolean onCreateOptionsMenu(Menu menu) {
-			super.onCreateOptionsMenu(menu);
-			MenuInflater inflater = getMenuInflater();
-			inflater.inflate(R.menu.settings, menu);
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.settings, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.version:
+			PackageInfo pinfo = null;
+			try {
+				pinfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+			} catch (NameNotFoundException e) {
+				e.printStackTrace();
+			}
+			int versionNumber = pinfo.versionCode;
+			String versionName = pinfo.versionName;
+
+			new AlertDialog.Builder( this )
+			.setTitle( "Pogo-Version" )
+			.setMessage("Application version: " + versionName)
+			.setIcon(android.R.drawable.ic_dialog_alert)
+			.setNegativeButton( "Okay", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					Log.d( tag, "Version exit" );
+				}
+			} )
+			.show();
+			return true;
+		case R.id.resetSettings:
+			new AlertDialog.Builder( this )
+			.setTitle( "Pogo-Reset" )
+			.setMessage("Do you want to reset game preferences?")
+			.setIcon(android.R.drawable.ic_dialog_alert)
+			.setNegativeButton( "No", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					Log.d( tag, "Reset negative" );
+				}
+			})
+			.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					Log.d(tag, "Reset positive");
+					settings = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+					SharedPreferences.Editor editor = settings.edit();
+					editor.clear();
+					//PreferenceManager.setDefaultValues(getBaseContext(), R.xml.settings, true);
+					PreferenceManager.setDefaultValues(getBaseContext(), R.xml.settings, false);
+					editor.commit();
+					setResult(3, data);
+					finish();
+				}
+			})
+			.show();
 			return true;
 		}
-	 
-	 @Override
-		public boolean onOptionsItemSelected(MenuItem item) {
-			switch (item.getItemId()) {
-			case R.id.version:
-				PackageInfo pinfo = null;
-				try {
-					pinfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-				} catch (NameNotFoundException e) {
-					e.printStackTrace();
-				}
-				int versionNumber = pinfo.versionCode;
-				String versionName = pinfo.versionName;
-
-				new AlertDialog.Builder( this )
-				.setTitle( "Pogo-Version" )
-				.setMessage("Application version: " + versionName)
-				.setIcon(android.R.drawable.ic_dialog_alert)
-				.setNegativeButton( "Okay", new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int which) {
-						Log.d( tag, "Version exit" );
-					}
-				} )
-				.show();
-				return true;
-			case R.id.resetSettings:
-				new AlertDialog.Builder( this )
-				.setTitle( "Pogo-Reset" )
-				.setMessage("Do you want to reset game preferences?")
-				.setIcon(android.R.drawable.ic_dialog_alert)
-				.setNegativeButton( "No", new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int which) {
-						Log.d( tag, "Reset negative" );
-					}
-				})
-				.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int which) {
-						Log.d(tag, "Reset positive");
-						settings = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-						SharedPreferences.Editor editor = settings.edit();
-						editor.clear();
-						//PreferenceManager.setDefaultValues(getBaseContext(), R.xml.settings, true);
-						PreferenceManager.setDefaultValues(getBaseContext(), R.xml.settings, false);
-						editor.commit();
-						setResult(3, data);
-						finish();
-					}
-				})
-				.show();
-				return true;
-			}
-			return false;
-		}
+		return false;
+	}
 
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
 			String key) {
+		if (key.equals("GAME_LANGUEGE")) {
+			String lang = sharedPreferences.getString("GAME_LANGUEGE", "");
+			if ("English".equals(lang)) {
+				lang = "en";
+			} else if ("Bulgarian".equals(lang)) {
+				lang = "bg";
+			}
+			
+			Locale local = new Locale(lang);
+			Locale.setDefault(local);
+			Configuration config = new Configuration();
+			config.locale = local;
+			getBaseContext().getResources().updateConfiguration(config, null);
+			setResult(4, data);
+		} else {
+			setResult(RESULT_OK, data);
+		}
 		Map<String, ?> prefs = sharedPreferences.getAll();
-
 		Log.d(tag, "Change " + key + " to " + prefs.get(key));
-		setResult(RESULT_OK, data);
 	}
 }
