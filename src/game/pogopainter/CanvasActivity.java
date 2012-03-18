7f9598a087c0e48907fa154a7f56915ea7476005
Asen Lekov <Asen Lekov@localhost>
2012-03-18 05:02:35 +0200

    0.5.1(20120318) + Added: layer for right controls - Missed layer for left + Changed: controls option from list to check button + fixed: bug in controls position selection + added: some assets + added: joystick + added: score list for players with indicatior icons

--- src/game/pogopainter/CanvasActivity.java
+++ src/game/pogopainter/CanvasActivity.java
@@ -1,14 +1,33 @@
 package game.pogopainter;
 
 import game.pogopainter.R;
+import game.system.Metrics;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout.LayoutParams;
 
 public class CanvasActivity extends Activity {
+	private String tag = "Canvas";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.canvas);
+		Metrics m = new Metrics();
+		Log.i(tag, Boolean.toString(m.isLeftControls()));
+		if (m.isLeftControls() == true) {
+			String pos = "LEFT CONTROLS";
+			Log.d(tag, "Load " + pos);
+			setContentView(R.layout.canvas_left_controls);
+		} else if (m.isLeftControls() == false) {
+			String pos = "RIGHT CONTROLS";
+			Log.d(tag, "Load " + pos);
+			setContentView(R.layout.canvas_right_controls);
+		}
+		View canvasPanel = findViewById(R.id.canvasPanel);
+		LayoutParams params = new LayoutParams(m.getScreenHeight(), m.getScreenHeight());
+		canvasPanel.setLayoutParams(params);
+		
 	}
 }
