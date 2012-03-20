1dd7b9f9fc1a59f5564ec05c7d2d67a2352033bf
Aleksandar Angelov <aleksandar.angelovv@gmail.com>
2012-03-20 15:58:04 +0200

    0.5.5(20120320) + Canvas init (90%) + Fixed Arrows + CanvasThread manipulation(still very buggy)

--- src/game/player/Actions.java
+++ src/game/player/Actions.java
@@ -6,33 +6,61 @@ import game.system.Direction;
 public class Actions {
 
 	public void move(Board b, Player player, Direction dir) {
-		boolean res = false;
+		int boardSize = b.getBoardSize();
 
 		switch (dir) {
 		case RIGHT:
-			if (player.getX() < b.getBoardSize()) {
+			if (checkDir(dir, player, boardSize)) {
 				player.setX(player.getX() + 1);
 				b.setPlayerColorOnCell(player);
 			}
 			break;
 		case UP:
-			if (player.getY() < b.getBoardSize()) {
+			if (checkDir(dir, player, boardSize)) {
 				player.setY(player.getY() -1);
 				b.setPlayerColorOnCell(player);
 			}
 			break;
 		case LEFT:
-			if (player.getX() > 1) {
+			if (checkDir(dir, player, boardSize)) {
 				player.setX(player.getX() -1);
 				b.setPlayerColorOnCell(player);
 			}
 			break;
 		case DOWN:
-			if (player.getY() < b.getBoardSize()) {
+			if (checkDir(dir, player, boardSize)) {
 				player.setY(player.getY() +1);
 				b.setPlayerColorOnCell(player);
 			}
 			break;
 		}
+	}
+	
+	public boolean checkDir(Direction dir, Player player, int boardSize) {
+		boolean res = false;
+		boardSize --;
+		switch(dir) {
+		case RIGHT:
+			if (player.getX() < boardSize) {
+				res = true;
+			}
+			break;
+		case UP:
+			if(player.getY() > 0) {
+				res = true;
+			}
+			break;
+		case LEFT:
+			if (player.getX() > 0) {
+				res = true;
+			}
+			break;
+		case DOWN:
+			if (player.getY() < boardSize) {
+				res = true;
+			}
+			break;
+		}
+		return res;
 	}
 }
