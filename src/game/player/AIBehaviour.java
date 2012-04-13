3916fdecb7e1ca23f17effaa998683003a9dad8b
Asen Lekov <asenlekoff@gmail.com>
2012-04-14 02:32:13 +0300

    0.11.6(20120414) ### PREPARING FOR AI REWRITE ### + Another set of methods for AI improvements + Again cosmetics - AI suck, have logical & code issues 	(no getting properly direction if theres no bonuses on board and more...) - AI need to be refreshed or fully rewrite

--- src/game/player/AIBehaviour.java
+++ src/game/player/AIBehaviour.java
@@ -4,7 +4,6 @@ import game.bonuses.Arrow;
 import game.bonuses.BonusObject;
 import game.bonuses.Checkpoint;
 import game.gametypes.Game;
-import game.system.Actions;
 import game.system.Board;
 import game.system.Cell;
 import game.system.Direction;
@@ -18,6 +17,7 @@ public class AIBehaviour implements Behaviour {
 	private Direction lastDir = Direction.NONE;
 	private Checkpoint random = null;
 	private Arrow arrow = null;
+	private Random rnd = new Random();
 
 	public AIBehaviour(Difficulty AIdifficult, Game actions) {
 		this.AIdifficult = AIdifficult;
@@ -26,7 +26,6 @@ public class AIBehaviour implements Behaviour {
 
 	public void easy(Board b,Player AI, int randomNumber) {
 		Direction nextDir = Direction.NONE;
-		Random rnd = new Random();
 		int check = rnd.nextInt(2)+1;
 		int goTo = rnd.nextInt(2)+1;
 
@@ -65,16 +64,48 @@ public class AIBehaviour implements Behaviour {
 		return lastDir;
 	}
 
+	private boolean shouldAIGetArrow(Player AI, Arrow arrow) {
+		boolean isReached = false;
+		for (Direction dir : Direction.values()) {
+			boolean checkDir = actions.checkDir(dir, AI, 8);
+			switch (dir) {
+			case LEFT:
+				if (checkDir && actions.getBonusHandler().checkArrowForGivingPoints(arrow)) {isReached = true;}
+				break;
+			case RIGHT:
+				if (checkDir && actions.getBonusHandler().checkArrowForGivingPoints(arrow)) {isReached = true;}
+				break;
+			case UP:
+				if (checkDir && actions.getBonusHandler().checkArrowForGivingPoints(arrow)) {isReached = true;}
+				break;
+			case DOWN:
+				if (checkDir && actions.getBonusHandler().checkArrowForGivingPoints(arrow)) {isReached = true;}
+				break;
+			}
+		}
+		return isReached;
+	}
+	
+	private Direction getNewFreshDirection(Board b, Player p, Direction l) {
+		Direction newDir = Direction.NONE;
+		while(!actions.checkDir(l, p, b.getBoardSize()) && (l == newDir || newDir == Direction.NONE)) {
+			newDir = Direction.values()[rnd.nextInt(4)+1];
+		}
+		return newDir;
+	}
+
 	private double calcDistance(int x, int y, Cell destination) {
 		double distance = Math.sqrt(Math.pow((x - destination.getX()), 2) + 
 				Math.pow((y - destination.getY()), 2));
-
 		return distance;
 	}
 
-	//TODO: check every neighbour cell distance to one of the checkpoints and 
-	//		return the direction with shortest distance to update!
-
+	private double calcDistance(Player AI, int x, int y) {
+		double distance = Math.sqrt(Math.pow((AI.getX() - x), 2) + 
+				Math.pow((AI.getY() - y), 2));
+		return distance;
+	}
+	
 	private Direction getNextDirectionToCheckpoint(Player AI, BonusObject bonus) {
 		Direction nextDirection = Direction.NONE;
 		int x = AI.getX();
@@ -83,28 +114,29 @@ public class AIBehaviour implements Behaviour {
 		double distance = calcDistance(x,y , destination);
 
 		for (Direction dir : Direction.values()) {
+			boolean checkDir = actions.checkDir(dir, AI, 8);
+			
 			switch (dir) {
 			case LEFT:
-				if (actions.checkDir(dir, AI, 8) && distance > calcDistance(x-1, y, destination)) {
+				if (checkDir && distance > calcDistance(x-1, y, destination)) {
 					distance = calcDistance(x-1, y, destination);
 					nextDirection = dir;
 				}
 				break;
 			case RIGHT:
-				if (actions.checkDir(dir, AI, 8) && distance > calcDistance(x+1, y, destination)) {
+				if (checkDir && distance > calcDistance(x+1, y, destination)) {
 					distance = calcDistance(x+1, y, destination);
 					nextDirection = dir;
 				}
-
 				break;
 			case DOWN:
-				if (actions.checkDir(dir, AI, 8) && distance > calcDistance(x, y+1, destination)) {
+				if (checkDir && distance > calcDistance(x, y+1, destination)) {
 					distance = calcDistance(x, y+1, destination);
 					nextDirection = dir;
 				}
 				break;
 			case UP:
-				if (actions.checkDir(dir, AI, 8) && distance > calcDistance(x, y-1, destination)) {
+				if (checkDir && distance > calcDistance(x, y-1, destination)) {
 					distance = calcDistance(x, y-1, destination);
 					nextDirection = dir;
 				}
