778c881e975c57a366592aeceff785a7fab457b6
Aleksandar Angelov <aleksandar.angelovv@gmail.com>
2012-04-30 11:39:42 +0300

    0.13.4(20120430) + Now arrow changing direction animation working + Little laggy need optimization + Refactored some code in Panel , Bonus Handler + Fix spawning chance for bonuses

--- src/tempest/game/pogopainter/bonuses/Arrow.java
+++ src/tempest/game/pogopainter/bonuses/Arrow.java
@@ -1,13 +1,21 @@
 package tempest.game.pogopainter.bonuses;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import tempest.game.pogopainter.player.Player;
 import tempest.game.pogopainter.system.Board;
 
 public class Arrow extends BonusObject {
-	int state = 0;
+	private int state = 0;
+	private int rotationDeg = 0;
+	private boolean rotated;
 
 	public Arrow() {
 		changeState();
+		rotated = true;
 		type = Bonuses.ARROW;
 	}
 
@@ -40,9 +48,58 @@ public class Arrow extends BonusObject {
 			break;
 		}
 	}
+	
+	@Override
+	public void draw(Canvas canvas, Paint paint, Rect bonusRectangle) {
+		Matrix mat = new Matrix();
+		Rect scaledRect = null;
+		
+		mat.postScale(scaleX, scaleY);
+
+		if (scaleX <= 1.0f) {
+			scaleX += 0.05f;
+			scaleY += 0.05f;
+		}
+		
+		int degree = (state - 1) * 90;
+		if (degree == 0) {
+			degree = 360;
+		}
+		if (!rotated) {
+			if (rotationDeg != degree) {
+				rotationDeg += 30;
+			}
+		} else {
+			rotationDeg = degree;
+		}
+		
+		mat.postRotate(rotationDeg);
+		Bitmap bitmap = Bitmap.createBitmap(bonusBitmap, 0, 0, 
+				bonusBitmap.getWidth(), bonusBitmap.getHeight(), mat, true);
+		
+		int top    = bonusRectangle.centerY()-(bitmap.getHeight()/2);
+		int left   = bonusRectangle.centerX()-(bitmap.getWidth()/2);
+		int bottom = bonusRectangle.centerY()+(bitmap.getHeight()/2);
+		int right  = bonusRectangle.centerX()+(bitmap.getWidth()/2);
+		
+		if(left < bonusRectangle.left) {
+			left   = bonusRectangle.left;
+			top    = bonusRectangle.top;
+			right  = bonusRectangle.right;
+			bottom = bonusRectangle.bottom;
+		}
+		scaledRect = new Rect(left, top, right, bottom);
+		
+		canvas.drawBitmap(bitmap, null, scaledRect, paint);
+	};
 
 	public void changeState() {
 		this.state++;
+		rotated = false;
+		rotationDeg = ((state - 1) * 90) - 90;
+		if (rotationDeg == -90) {
+			rotationDeg = 270;
+		}
 		if (state == 5) state = 1;
 	}
 
